85fb3a27635ccd73b5947a9b25d476181adf38f0
Denys Gorozhanin <denys.gorozhanin@bela.de>
2024-11-12 09:36:40 +0100

    Карта + инциденты и траффик + Кастомная локация

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useEffect, useRef } from "react";
-import tt, { Map as TomTomMap } from "@tomtom-international/web-sdk-maps"; // Импортируем SDK и тип Map
-import "@tomtom-international/web-sdk-maps/dist/maps.css"; // Импортируем стили карты
+import tt, { Map as TomTomMap } from "@tomtom-international/web-sdk-maps";
+import "@tomtom-international/web-sdk-maps/dist/maps.css";
 
 interface MapProps {
   apiKey: string | undefined;
@@ -11,28 +11,61 @@ interface MapProps {
 }
 
 const Map = ({ apiKey, lat, lng, zoom }: MapProps) => {
-  const mapElement = useRef<HTMLDivElement | null>(null); // Реф для HTML элемента карты
-  const mapInstance = useRef<TomTomMap | null>(null); // Реф для инстанса карты с правильным типом
+  const mapElement = useRef<HTMLDivElement | null>(null);
+  const mapInstance = useRef<TomTomMap | null>(null);
 
   useEffect(() => {
     if (mapElement.current && !mapInstance.current && apiKey) {
+      // Инициализация карты
       mapInstance.current = tt.map({
-        key: apiKey, // Ваш API ключ
-        container: mapElement.current, // HTML-элемент для рендера карты
-        center: [lng, lat], // Начальные координаты
-        zoom: zoom, // Начальный уровень зума
-        pitch: 45, // Угол наклона карты
+        key: apiKey,
+        container: mapElement.current,
+        center: [lng, lat],
+        zoom: zoom,
+        pitch: 45,
         stylesVisibility: {
           map: true,
           trafficFlow: true,
           trafficIncidents: true,
         },
       });
+
+      // Координаты кастомной локации
+      const customCoordinates = [10.10994, 54.326166];
+
+      // Создание маркера
+      const marker = new tt.Marker()
+        .setLngLat(customCoordinates)
+        .addTo(mapInstance.current);
+
+      // Создание попапа с кастомным содержимым
+      const popupOffsets = {
+        top: [0, 0],
+        bottom: [0, -70],
+        "bottom-right": [0, -70],
+        "bottom-left": [0, -70],
+        left: [25, -35],
+        right: [-25, -35],
+      };
+
+      const popupContent = `
+        <div style="text-align: center;">
+          Adresse, г. Kiel
+        </div>
+      `;
+
+      const popup = new tt.Popup({ offset: popupOffsets }).setHTML(
+        popupContent
+      );
+
+      // Прикрепление попапа к маркеру
+      marker.setPopup(popup).togglePopup();
     }
 
+    // Удаление карты при размонтировании
     return () => {
       if (mapInstance.current) {
-        mapInstance.current.remove(); // Убираем карту при размонтировании компонента
+        mapInstance.current.remove();
       }
     };
   }, [apiKey, lat, lng, zoom]);
